d9a4da556d2149262c08ba7d80650ce262831039
Nisarahmed Hidayeti <Nisarahmedhidayeti@gmail.com>
2026-08-02 15:22:54 +0300

    writing the age

--- Age guesser.py
+++ Age guesser.py
@@ -1,15 +1,28 @@
-
+from datetime import date
 
 while 1:
     Birth_year = input("What is your birth year")
-
     try:
         Birth_year = int(Birth_year)
         break
     except ValueError:
-        print("Please enter a valid date")
+        print("Please enter a valid Year")
 
+while 1:
+    Birth_Month = input("What is your birth Month")
+    try:
+        Birth_Month = int(Birth_Month)
+        break
+    except ValueError:
+        print("Please enter a valid Month")
 
+while 1:
+    Birth_day = input("What is your birth Day")
+    try:
+        Birth_day = int(Birth_day)
+        break
+    except ValueError:
+        print("Please enter a valid Day")
 
-Birth_Month = input("What is your birth Month")
-Birth_day = input("What is your birth day")
+date = date.today()
+print(f"You are {date.year - Birth_year} years, {date.month - Birth_Month} months, and {date.day - Birth_day} days old.")
